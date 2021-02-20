6f87cd29bfb38340ab1ac1307b2e3a26e0de88a0
lafBot <64456835+lafBot@users.noreply.github.com>
2021-02-20 16:08:12 -0700

    Jobs page component completed function

--- frontend/src/jobs/Jobs.js
+++ frontend/src/jobs/Jobs.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import JoblyApi from "../api/api";
+import SearchForm from "../SearchForm";
+import JobCardList from "./JobCardList";
 
-function Jobs() {
+const Jobs = () => {
+    const [jobs, setJobs] = useState(null);
+
+    useEffect(() => {
+        search()
+    }, []);
+
+    async function search(title) {
+        let jobsList = await JoblyApi.getJobs(title);
+        setJobs(jobsList);
+    }
     
+    if (!jobs) return <h1>Loading...</h1>
+
     return (
-        <h1>Jobs</h1>
+        <div className="Jobs">
+            <SearchForm search={search} />
+            {jobs.length
+                ? <JobCardList jobs={jobs} />
+                : <p>No results found.</p>
+            }
+        </div>
 
     )
 }
