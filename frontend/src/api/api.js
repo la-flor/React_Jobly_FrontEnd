d8503aebafa032e317d310fda25c6e5841999fdb
lafBot <64456835+lafBot@users.noreply.github.com>
2021-02-19 20:28:01 -0700

    added getCurrentUser()

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -59,7 +59,10 @@ class JoblyApi {
   /* Get token for login from username, password. */
 
   static async login(data) {
+
+    debugger;
     let res = await this.request(`auth/token`, data, "post");
+
     return res.token;
   }
 
@@ -70,16 +73,16 @@ class JoblyApi {
     return res.token;
   }
 
+  static async getCurrentUser(username) {
+    let res = await this.request(`users/${username}`);
+    return res.user;
+  }
+
   static async getJobs(title) {
     let res = await this.request("jobs", { title });
     return res.jobs;
   }
 
 }
-
-// for now, put token ("testuser" / "password" on class)
-// JoblyApi.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
-//     "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
-//     "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
 
 export default JoblyApi;
