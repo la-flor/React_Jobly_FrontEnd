b5688634992d169ca3df7a1a4629aacc2754d496
lafBot <64456835+lafBot@users.noreply.github.com>
2021-02-21 11:16:00 -0700

    Added homepage

---
+++ frontend/src/homepage/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./HomePage.css";
+
+const HomePage = () => {
+    return (
+        <div className="HomePage mt-5">
+            <h1 className="HomePage-title">Jobly</h1>
+            <p className="HomePage-slogan">All the jobs in one, convienient place</p>
+        </div>
+    )
+}
+
+export default HomePage;
